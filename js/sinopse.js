19ed845d6e4389f4c72a91698d7126d5dcd74af1
Leofront-end <ls171258@gmail.com>
2025-06-01 19:27:21 -0300

    Fazendo a sinopse

--- js/sinopse.js
+++ js/sinopse.js
@@ -1,11 +1,20 @@
 let bannerHorizontal = document.getElementById('bannerHorizontal')
 let bannerVertical = document.getElementById('bannerVertical')
 let descricao = document.getElementById('descricao')
+let informacoes = document.getElementById("informacoes")
+let tempo = document.getElementById("tempo")
+let diretor = document.getElementById("diretor")
 
 let valor = localStorage.getItem("URL")
 valor = JSON.parse(valor)
 console.log(valor)
 
-bannerHorizontal.src = `${valor.movie_banner}`
+let horas = Math.floor(valor.running_time / 60)
+let minutos = Math.floor(valor.running_time % 60)
+
+informacoes.style.background = `center / cover no-repeat url(${valor.movie_banner}) `
+
 bannerVertical.src = `${valor.image}`
 descricao.textContent = `${valor.description}`
+tempo.textContent = `${horas} horas e ${minutos} minutos`
+diretor.textContent = `${valor.director}`
